bcb454aca711b5c160593e8518cdbc53a2997a7d
Henrique Lovisi Ennes <hlovisiennes@gmail.com>
2026-09-03 16:38:48 +0200

    Fix type bug in torus

--- src/python/test/test_liedetect_t_2.py
+++ src/python/test/test_liedetect_t_2.py
@@ -1,119 +1,143 @@
-""" This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
-    See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
-    Author(s):       Henrique Ennes & Raphaël Tinarrage
+"""This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
+See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
+Author(s):       Henrique Ennes & Raphaël Tinarrage
 
-    Copyright (C) 2016 Inria
+Copyright (C) 2016 Inria
 """
-import pytest
-import numpy as np
 
+import pytest
+from gudhi import liedetect
+from gudhi.datasets import linear_orbits
 from gudhi.liedetect import OrbitFitter
-'''
+
+"""
 Fixes data set.
-'''
+"""
+
+rep_type = ((1, 2), (2, 3))
 
 
 @pytest.fixture
 def fix_pts():
-    '''
+    """
     Builds orbit of weights 1 in R^4.
-    '''
-    thetas1 = np.linspace(0, 2*np.pi, 100)
-    thetas2 = np.linspace(0, 2*np.pi, 100)
-    return np.array([(np.cos(theta1), np.sin(theta1),
-                      np.cos(theta2), np.sin(theta2))
-                    for theta1 in thetas1
-                    for theta2 in thetas2])
+    """
+    group = "torus"
+    seed = 42
+    nb_points = 500
+
+    return linear_orbits.sample_from_lie_group_rep(
+        group=group,
+        rep_type=rep_type,
+        nb_points=nb_points,
+        seed=seed,
+    )
 
 
-'''
+"""
 Test functions
-'''
+"""
 
 
 def test_lie_pca(fix_pts):
+    # Test PCA method for lie_pca
+    # As group dim is 2, only two eigenvalue should be small, the others should be large
     orbit_fitter = OrbitFitter(fix_pts)
-    nb_neighbors = 10
-    orbit_dim = 1
+    nb_neighbors = 30
+    orbit_dim = 2
 
-    _ = orbit_fitter.lie_pca(nb_neighbors=nb_neighbors,
-                             orbit_dim=orbit_dim,
-                             method='PCA',
-                             verbose=False)
+    _ = orbit_fitter.lie_pca(
+        nb_neighbors=nb_neighbors, orbit_dim=orbit_dim, method="PCA", verbose=False
+    )
     vals = orbit_fitter.print_lie_pca_eigenvalues(return_vals=True)
-    assert abs(vals[0]) < 0.15
-    assert abs(vals[1]) < 0.15
+    assert abs(vals[0]) < 0.015
+    assert abs(vals[1]) < 0.015
 
     for i in vals[2:]:
-        assert abs(i) > 0.15
+        assert abs(i) > 0.015
 
+    # Test covariance method for lie_pca
     orbit_fitter = OrbitFitter(fix_pts)
-    nb_neighbors = 10
-    orbit_dim = 1
+    nb_neighbors = 30
+    orbit_dim = 2
 
-    _ = orbit_fitter.lie_pca(nb_neighbors=nb_neighbors,
-                             orbit_dim=orbit_dim,
-                             method='covariance',
-                             verbose=False)
+    _ = orbit_fitter.lie_pca(
+        nb_neighbors=nb_neighbors,
+        orbit_dim=orbit_dim,
+        method="covariance",
+        verbose=False,
+    )
     vals = orbit_fitter.print_lie_pca_eigenvalues(return_vals=True)
-    assert abs(vals[0]) < 0.15
-    assert abs(vals[1]) < 0.15
+    assert abs(vals[0]) < 0.015
+    assert abs(vals[1]) < 0.015
 
     for i in vals[2:]:
-        assert abs(i) > 0.15
+        assert abs(i) > 0.015
 
 
 def test_project_lie_algebra(fix_pts):
+    # Teste if we found the right rep type
     orbit_fitter = OrbitFitter(fix_pts)
     nb_neighbors = 30
     orbit_dim = 2
 
-    _ = orbit_fitter.lie_pca(nb_neighbors=nb_neighbors,
-                             orbit_dim=orbit_dim,
-                             method='PCA',
-                             verbose=False)
+    _ = orbit_fitter.lie_pca(
+        nb_neighbors=nb_neighbors, orbit_dim=orbit_dim, method="PCA", verbose=False
+    )
 
     frequency_max = 4
 
     method = "abelian"
-    la, _ = orbit_fitter.closest_algebra(group='torus', group_dim=2,
-                                         frequency_max=frequency_max,
-                                         method=method, verbose=False)
+    la, _ = orbit_fitter.closest_algebra(
+        group="torus",
+        group_dim=2,
+        frequency_max=frequency_max,
+        method=method,
+        verbose=False,
+    )
 
-    print(la)
-    assert abs(sum(la[0])) == 1
-    assert abs(sum(la[1])) == 1
+    assert liedetect.are_representations_equivalent("torus", rep_type, la)
 
     method = "bottom_lie_pca"
-    la, _ = orbit_fitter.closest_algebra(group='torus', group_dim=2,
-                                         frequency_max=frequency_max,
-                                         method=method, verbose=False)
-    assert abs(sum(la[0])) == 1
-    assert abs(sum(la[1])) == 1
+    la, _ = orbit_fitter.closest_algebra(
+        group="torus",
+        group_dim=2,
+        frequency_max=frequency_max,
+        method=method,
+        verbose=False,
+    )
+    assert liedetect.are_representations_equivalent("torus", rep_type, la)
 
     method = "full_lie_pca"
-    la, _ = orbit_fitter.closest_algebra(group='torus', group_dim=2,
-                                         frequency_max=frequency_max,
-                                         method=method, verbose=False)
-    assert abs(sum(la[0])) == 1
-    assert abs(sum(la[1])) == 1
+    la, _ = orbit_fitter.closest_algebra(
+        group="torus",
+        group_dim=2,
+        frequency_max=frequency_max,
+        method=method,
+        verbose=False,
+    )
+    assert liedetect.are_representations_equivalent("torus", rep_type, la)
 
 
 def test_sample_orbit(fix_pts):
+    # Test if we reconstructed an orbit close to the true one
     orbit_fitter = OrbitFitter(fix_pts)
     nb_neighbors = 30
     orbit_dim = 2
 
-    _ = orbit_fitter.lie_pca(nb_neighbors=nb_neighbors,
-                             orbit_dim=orbit_dim,
-                             method='PCA',
-                             verbose=False)
+    _ = orbit_fitter.lie_pca(
+        nb_neighbors=nb_neighbors, orbit_dim=orbit_dim, method="PCA", verbose=False
+    )
 
     frequency_max = 4
     method = "abelian"
-    _, _ = orbit_fitter.closest_algebra(group='torus', group_dim=2,
-                                        frequency_max=frequency_max,
-                                        method=method, verbose=False)
+    _, _ = orbit_fitter.closest_algebra(
+        group="torus",
+        group_dim=2,
+        frequency_max=frequency_max,
+        method=method,
+        verbose=False,
+    )
 
     _ = orbit_fitter.sample_orbit(nb_points=5000)
 
